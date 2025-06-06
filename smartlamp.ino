9f74021a54d8a84b481f8f858f03c04871333088
LUAN ALBUQUERQUE DOS SANTOS <luan.santos@indt.org.br>
2025-06-06 17:28:39 -0400

    Task 2.1 - Added SET_LED and GET_LDR

--- smartlamp.ino
+++ smartlamp.ino
@@ -1,12 +1,12 @@
 // Defina os pinos de LED e LDR
 // Defina uma variável com valor máximo do LDR (4000)
 // Defina uma variável para guardar o valor atual do LED (10)
-int ledPin; //Definir Pino
-int ledValue; 
+int ledPin = ; //Definir Pino
+int ledValue = 0; 
 
-int ldrPin; //Definir Pino
+int ldrPin = A0; //Definir Pino
 // Faça testes no sensor ldr para encontrar o valor maximo e atribua a variável ldrMax
-int ldrMax ;
+int ldrMax = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -14,7 +14,7 @@ void setup() {
     pinMode(ledPin, OUTPUT);
     pinMode(ldrPin, INPUT);
     
-    Serial.printf("SmartLamp Initialized.\n");
+    Serial.print("SmartLamp Initialized.\n");
 
 
 }
@@ -28,21 +28,34 @@ void loop() {
 
 void processCommand(String command) {
     // compare o comando com os comandos possíveis e execute a ação correspondente    
+    
+    if(command.equals("SET_LED")){
+        // Extrair o valor 
+        ledUpdate(0)
+        
+    }
 
     if (command.equals("GET_LED")) { // Retornar a intensidade atual do LED
-        Serial.println(ledValue);
+          Serial.printf("RES GET_LED ");
+          Serial.println(ledValue);
     } 
+
+    if (command.equals("GET_LDR")) { 
+
+          int ldrValue = ldrGetValue();
+          Serial.printf("RES GET_LDR ");
+          Serial.println(ldrValue);
+    } 
+   
 }
 
 // Função para atualizar o valor do LED
-void ledUpdate() {
+void ledUpdate(int newvalue) {
     // Valor deve convertar o valor recebido pelo comando SET_LED para 0 e 255
     // Normalize o valor do LED antes de enviar para a porta correspondente
-    int value=analogRead(ldrPin);
-    if(value >= ldrMax){
-      return 255;
-    }
-    return value*255/ldrMax;
+    int ledNewValue = (newvalue * 255) / 100;
+    analogWrite(ledPin, ledNewValue);
+
 }
 
 // Função para ler o valor do LDR
@@ -50,5 +63,14 @@ int ldrGetValue() {
     // Leia o sensor LDR e retorne o valor normalizado entre 0 e 100
     // faça testes para encontrar o valor maximo do ldr (exemplo: aponte a lanterna do celular para o sensor)       
     // Atribua o valor para a variável ldrMax e utilize esse valor para a normalização
-       Serial.println(ledValue);
+
+    int lrdValue = analogRead(ldrPin);
+
+    lrdValue = constrain(lrdValue, 0, ldrMax);
+    
+    // Normaliza para 0–100 
+    int normalized = (lrdValue * 100) / ldrMax;
+
+    return normalized;
+  
 }
