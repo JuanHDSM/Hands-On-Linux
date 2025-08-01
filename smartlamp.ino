ca6f4b982778f8447874a123a169ad7dcde86bcc
LUAN ALBUQUERQUE DOS SANTOS <luan.santos@indt.org.br>
2025-08-01 13:27:01 -0400

    adição de DHT no firmware

--- smartlamp.ino
+++ smartlamp.ino
@@ -1,10 +1,18 @@
+#include <DHT.h>
+
 // Defina os pinos de LED e LDR
-int ledPin = 2;
-int ldrPin = 34;
+int ledPin = 22;
+int ldrPin = 2;
 int ldrMax = 4000;  // valor máximo calibrado do LDR
 int ledValue = 10;  // valor de 0 a 100
 
+#define DHTTYPE DHT11
+#define DHTPIN 4
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
+  dht.begin();
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(ldrPin, INPUT);
@@ -13,18 +21,11 @@ void setup() {
 }
 
 void loop() {
-  int num =0;
   if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
     processCommand(command);
-    
   }
-  // delay(100);
-  // num+=100;
-
-  // if(num%1000){
-  //  Serial.printf("ta conectado e funcionando :) \n");
-  // }
+  delay(100);
 }
 
 void processCommand(String command) {
@@ -48,6 +49,22 @@ void processCommand(String command) {
       Serial.println("RES SET_LED 1");
     } else {
       Serial.println("RES SET_LED -1");
+    }
+  }
+  else if (command == "GET_TEMP") {
+    float temperatura = dht.readTemperature();
+    if (isnan(temperatura)) {
+      Serial.println("ERR GET_TEMP Sensor error");
+    } else {
+      Serial.printf("RES GET_TEMP %.1f\n", temperatura);  // °C
+    }
+  }
+  else if (command == "GET_HUM") {
+    float umidade = dht.readHumidity();
+    if (isnan(umidade)) {
+      Serial.println("ERR GET_HUM Sensor error");
+    } else {
+      Serial.printf("RES GET_HUM %.1f\n", umidade);  // %
     }
   }
   else {
